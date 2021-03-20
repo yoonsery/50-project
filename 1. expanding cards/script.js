e2fea6e0041e1fd427a6993b0aeab523de375283
yoonsery <yoon.sery@icloud.com>
2021-03-20 17:47:38 +0900

    Done: expanding cards

---
+++ 1. expanding cards/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const panels = document.querySelectorAll('.panel');
+
+// console.log(panels);
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
+});
+
+
+function removeActiveClasses() {
+  panels.forEach(panel => {
+    panel.classList.remove('active');
+  });
+}
