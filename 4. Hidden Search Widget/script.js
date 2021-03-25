9b3b86a5a30153b3f13d7eda538da0ef9fa04f74
yoonsery <yoon.sery@icloud.com>
2021-03-26 01:28:32 +0900

    Done!

--- 4. Hidden Search Widget/script.js
+++ 4. Hidden Search Widget/script.js
@@ -1,1 +1,11 @@
+'use strict';
 
+const $search = document.querySelector('.search');
+const $input = document.querySelector('.input');
+const $btn = document.querySelector('.btn');
+
+$btn.addEventListener('click', () =>{
+  $search.classList.toggle('active');
+  $input.value = '';
+  $input.focus();
+});
