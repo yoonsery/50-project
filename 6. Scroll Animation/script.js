f0a7ad701fb6f3c694a78798c55ad78d10711f9b
yoonsery <yoon.sery@icloud.com>
2021-03-28 01:49:52 +0900

    Scroll Animation
    all done adding js

---
+++ 6. Scroll Animation/script.js
@@ -0,0 +1,17 @@
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkboxes);
+
+// checkboxes(); 스크롤 전에 실행해놓아도 되고 up to you~
+function checkboxes() {
+  const triggerBottom = innerHeight / 5 * 4;
+
+  boxes.forEach(box => {
+    const boxTop = box.getBoundingClientRect().top;
+    if (boxTop < triggerBottom) {
+      box.classList.add('show');
+    } else {
+      box.classList.remove('show');
+    }
+  })
+}
