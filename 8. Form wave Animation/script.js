c0e6ef682b9fb86021eb05146492edb9bd3f93f3
yoonsery <yoon.sery@icloud.com>
2021-03-29 19:53:14 +0900

    Wavy Effect Done!

--- 8. Form wave Animation/script.js
+++ 8. Form wave Animation/script.js
@@ -3,6 +3,6 @@ const labels = document.querySelectorAll('.form-control label');
 labels.forEach(label => {
   label.innerHTML = label.innerText  // e,m,a,i,l 로 쪼갬
     .split('') // array로 쪼개서 가져옴
-    .map((letter, idx) => `<span>${letter}</span>`) // <span>E</span>...
+    .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`) // <span>E</span>...
     .join('') // 다시 스트링으로 가져옴
 });
