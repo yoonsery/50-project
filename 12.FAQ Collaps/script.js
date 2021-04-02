c981a936c7086d3c3951391f4a7055c40531ab01
yoonsery <yoon.sery@icloud.com>
2021-04-03 02:26:55 +0900

    add event in js

---
+++ 12.FAQ Collaps/script.js
@@ -0,0 +1,8 @@
+const $faq = document.querySelectorAll('.faq');
+const $toggles = document.querySelectorAll('.faq-toggle');
+
+$toggles.forEach(btn => {
+  btn.addEventListener('click', () => {
+    btn.parentNode.classList.toggle('active');
+  });
+});
