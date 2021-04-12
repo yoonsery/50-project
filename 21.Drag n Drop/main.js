e838d7eda8b7b9d913547b02ddacaaeb92d9bf64
yoonsery <yoon.sery@icloud.com>
2021-04-12 16:45:42 +0900

    add eventListener

--- 21.Drag n Drop/main.js
+++ 21.Drag n Drop/main.js
@@ -5,9 +5,19 @@ const $empties = document.querySelectorAll('.empty');
 $fill.addEventListener('dragstart', dragStart);
 $fill.addEventListener('dragend', dragEnd);
 
-// $empties.forEach((empty) => {
-//   empty;
-// });
+$empties.forEach((empty) => {
+  empty.addEventListener('dragover', dragOver);
+  empty.addEventListener('dragenter', dragEnter);
+  empty.addEventListener('dragleave', dragLeave);
+  empty.addEventListener('drop', dragDrop);
+});
+
+// for (const empty of $empties) {
+//   empty.addEventListener('dragover', dragOver);
+//   empty.addEventListener('dragenter', dragEnter);
+//   empty.addEventListener('dragleave', dragLeave);
+//   empty.addEventListener('drop', dragDrop);
+// }
 
 function dragStart() {
   console.log('drag start');
