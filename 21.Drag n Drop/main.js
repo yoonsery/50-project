6fb091a1bc92fb922efe9f7c5538ea958e7af455
yoonsery <yoon.sery@icloud.com>
2021-04-12 16:35:45 +0900

    make functions

---
+++ 21.Drag n Drop/main.js
@@ -0,0 +1,30 @@
+const $empties = document.querySelectorAll('.empty');
+const $fill = document.querySelector('.fill');
+
+$empties.forEach((empty) => {
+  empty;
+});
+
+function dragStart() {
+  console.log('drag start');
+}
+
+function dragEnd() {
+  console.log('drag end');
+}
+
+function dragOver() {
+  console.log('drag over');
+}
+
+function dragEnter() {
+  console.log('drag enter');
+}
+
+function dragLeave() {
+  console.log('drag leave');
+}
+
+function dragDrop() {
+  console.log('drag drop');
+}
