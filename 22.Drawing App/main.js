f15cd1a9d4df6de8edc58d265c541733a7c28f26
yoonsery <yoon.sery@icloud.com>
2021-04-14 00:08:28 +0900

    finish draw func

--- 22.Drawing App/main.js
+++ 22.Drawing App/main.js
@@ -24,7 +24,13 @@ $canvas.addEventListener('mousemove', (e) => {
     const x2 = e.offsetX;
     const y2 = e.offsetY;
 
-    console.log(x2, y2);
+    drawCircle(x2, y2);
+    // 마우스 빠르게 움직이면 동그라미 찍히지 않고 선으로 연결되게 drawLine 추가
+    drawLine(x, y, x2, y2);
+    // 부채꼴 모양처럼 면으로 뒤덮이는거 방지하기 위해 x = x2 넣어줌
+    x = x2;
+    y = y2;
+    // 선이 생기긴 하는데 선이 원의 지름보다 작아서 면봉처럼 보이는 현상
   }
 });
 
@@ -40,7 +46,6 @@ function drawLine(x1, y1, x2, y2) {
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
   ctx.strokeStyle = color;
-  ctx.lineWidth = size;
-  ctx.lineCap = 'round';
+  ctx.lineWidth = size * 2; // 선이 그으면 선이 원의 지름보다 작아서 면봉처럼 보이는데 ark에서 반지름을 설정해줬으니 * 2하면 된다
   ctx.stroke();
 }
