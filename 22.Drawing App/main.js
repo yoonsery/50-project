ba8252b5809e0376787f2c11f5a863996efb4efa
yoonsery <yoon.sery@icloud.com>
2021-04-13 23:57:41 +0900

    add mousemove event

--- 22.Drawing App/main.js
+++ 22.Drawing App/main.js
@@ -19,6 +19,15 @@ $canvas.addEventListener('mouseup', (e) => {
   y = undefined;
 });
 
+$canvas.addEventListener('mousemove', (e) => {
+  if (isPressed) {
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
+
+    console.log(x2, y2);
+  }
+});
+
 function drawCircle(x, y) {
   ctx.beginPath();
   ctx.arc(x, y, size, 0, Math.PI * 2);
