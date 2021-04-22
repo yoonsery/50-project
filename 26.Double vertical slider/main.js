1031d1b4698537db9380e4a62cf2f9909fc3b044
yoonsery <yoon.sery@icloud.com>
2021-04-22 23:24:52 +0900

    done with css

---
+++ 26.Double vertical slider/main.js
@@ -0,0 +1,6 @@
+const sliderContainer = document.querySelector('.slider-container');
+const sliderRight = document.querySelector('.right-slide');
+const sliderLeft = document.querySelector('.left-slide');
+const upButton = document.querySelector('.up-button');
+const downButton = document.querySelector('.down-button');
+const slidesLength = sliderRight.querySelectorAll('div').length;
