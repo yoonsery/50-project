65db8c5e28f823a88d58de341e234991f083f4cd
yoonsery <yoon.sery@icloud.com>
2021-04-25 22:41:41 +0900

    add getting repos

--- 28.Github profile/main.js
+++ 28.Github profile/main.js
@@ -8,10 +8,20 @@ async function getUser(username) {
   try {
     const { data } = await axios(APIURL + username);
     createUserCard(data);
+    getRepos(username);
   } catch (err) {
     if (err.response.status == 404) {
       createErrorCard('No profile with this username');
     }
+  }
+}
+
+async function getRepos(username) {
+  try {
+    const { data } = await axios(APIURL + username + '/repos?sort=created');
+    addReposToCard(data);
+  } catch (err) {
+    createErrorCard('Problem fetching repos');
   }
 }
 
@@ -42,12 +52,27 @@ function createUserCard(user) {
   main.innerHTML = cardHTML;
 }
 
-async function createErrorCard(message) {
+function createErrorCard(message) {
   const cardHTML = `
     <div class="card">
       <h1>${message}</h1>
     </div>`;
   main.innerHTML = cardHTML;
+}
+
+function addReposToCard(repos) {
+  const reposEl = document.getElementById('repos');
+
+  // repo가 많은 경우를 대비해서 5개까지만 잘라내서 보여줌
+  repos.slice(0, 5).forEach((repo) => {
+    const repoEl = document.createElement('a');
+    repoEl.classList.add('repo');
+    repoEl.href = repo.html_url;
+    repoEl.target = '_blank';
+    repoEl.innerText = repo.name;
+
+    reposEl.appendChild(repoEl);
+  });
 }
 
 form.addEventListener('submit', (e) => {
