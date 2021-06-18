77ebe8c2cdc2d5dad3ca20c430bf88099837cdfe
yoonsery <yunsel2@gmail.com>
2021-06-19 00:31:14 +0900

    add scale

--- 44.Custom Range Slider/main.js
+++ 44.Custom Range Slider/main.js
@@ -6,12 +6,23 @@ range.addEventListener('input', (e) => {
 
   const range_width = getComputedStyle(e.target).getPropertyValue('width');
   const label_width = getComputedStyle(label).getPropertyValue('width');
+
   const num_range_width = +range_width.substring(0, range_width.length - 2);
   const num_label_width = +label_width.substring(0, label_width.length - 2);
-  const max = +e.target.max;
 
-  const left = value * (num_range_width / max) - num_label_width / 2;
+  const max = +e.target.max;
+  const min = +e.target.min;
+
+  const left =
+    value * (num_range_width / max) -
+    num_label_width / 2 +
+    scale(value, min, max, 10, -10);
+
+  label.style.left = `${left}px`;
 
   label.textContent = value;
-  label.style.left = `${left}px`;
 });
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
