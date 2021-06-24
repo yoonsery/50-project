e95a31dd28fd09de2f8a62952da13ce703b285a2
yoonsery <yunsel2@gmail.com>
2021-06-24 12:40:52 +0900

    clean up

--- 06.Scroll Animation/script.js
+++ 06.Scroll Animation/script.js
@@ -2,16 +2,16 @@ const boxes = document.querySelectorAll('.box');
 
 window.addEventListener('scroll', checkboxes);
 
-// checkboxes(); 스크롤 전에 실행해놓아도 되고 up to you~
+checkboxes(); // 스크롤 전에 실행해놓아도 되고 up to you~
 function checkboxes() {
-  const triggerBottom = innerHeight / 5 * 4;
+  const triggerBottom = (innerHeight / 5) * 4;
 
-  boxes.forEach(box => {
+  boxes.forEach((box) => {
     const boxTop = box.getBoundingClientRect().top;
     if (boxTop < triggerBottom) {
       box.classList.add('show');
     } else {
       box.classList.remove('show');
     }
-  })
+  });
 }
