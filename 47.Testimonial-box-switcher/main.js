2a145115a72256c43180c44c7e45aeeec99456c1
yoonsery <yunsel2@gmail.com>
2021-06-26 02:24:09 +0900

    make json file add js

---
+++ 47.Testimonial-box-switcher/main.js
@@ -0,0 +1,32 @@
+const testimonialsContainer = document.querySelector('.testimonial-container');
+const testimonial = document.querySelector('.testimonial');
+const userImage = document.querySelector('.user-image');
+const username = document.querySelector('.username');
+const role = document.querySelector('.role');
+
+async function loadTestimonials() {
+  const data = await fetch('./data/data.json');
+  const result = await data.json();
+  return result;
+}
+
+let idx = 1;
+loadTestimonials() //
+  .then((result) => {
+    setInterval(() => updateTestimonial(result), 10000);
+  });
+
+function updateTestimonial(item) {
+  const { name, position, photo, text } = item[idx];
+
+  testimonial.innerHTML = text;
+  userImage.src = photo;
+  username.innerHTML = name;
+  role.innerHTML = position;
+
+  idx++;
+
+  if (idx > item.length - 1) {
+    idx = 0;
+  }
+}
