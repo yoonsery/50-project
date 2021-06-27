477db14f8360f1361c97949f5b47f405accca77c
yoonsery <yunsel2@gmail.com>
2021-06-28 00:21:37 +0900

    clean up

--- 48. Random Image Feed/main.js
+++ 48. Random Image Feed/main.js
@@ -2,21 +2,20 @@ const container = document.querySelector('.container');
 const unsplashURL = 'https://source.unsplash.com/random/';
 const inputNumber = document.querySelector('.image-number');
 const loadImgBtn = document.querySelector('.load-image');
-const btncontainer = document.querySelector('.button-container');
 const rows = 5;
 
-getImage(rows);
+getImages(rows);
 
 loadImgBtn.addEventListener('click', (e) => {
   container.innerHTML = ``;
   const rows = inputNumber.value / 3;
-  getImage(rows);
+  getImages(rows);
   console.log(3, rows);
 });
 
 inputNumber.addEventListener('keydown', (e) => e.preventDefault());
 
-function getImage(rows) {
+function getImages(rows) {
   for (let i = 0; i < rows * 3; i++) {
     const img = document.createElement('img');
     img.src = `${unsplashURL}${getRandomSize()}`;
