c6c09c9fcd428a275fcab62d5963ca93fda3ca3a
sery <yunsel2@gmail.com>
2021-11-11 15:32:57 +0900

    refactor: Bug fix when using han-geul

--- 13.Random Choice  Picker/script.js
+++ 13.Random Choice  Picker/script.js
@@ -3,30 +3,35 @@ const $tagsElement = document.getElementById('tags');
 
 $textarea.focus();
 
-
 // 한글로 입력하면 에러가 난당...왤까
-$textarea.addEventListener('keyup', (e) =>{
+$textarea.addEventListener('keyup', (e) => {
+  if (e.isComposing) {
+    return;
+  }
   createTags(e.target.value);
 
   if (e.key === 'Enter') {
     setTimeout(() => {
       e.target.value = '';
-    }, 10)
+    }, 10);
     randomSelect();
   }
-})
+});
 
 function createTags(input) {
   // console.log(input);
   // split , put in array
-  const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim());
+  const tags = input
+    .split(',')
+    .filter((tag) => tag.trim() !== '')
+    .map((tag) => tag.trim());
   //공백이 아니라면 그 값을 리턴할 거야, filter에 컨디션 걸어서 그에 맞는 필터값 리턴, 만약 배열에 빈문자열이 있다면 빈문자열 요소를 없앰
   // 문자열 스트링에 공백있는 걸 없앰
   // console.log(tags);
 
   $tagsElement.innerHTML = '';
 
-  tags.forEach(tag => {
+  tags.forEach((tag) => {
     const tagEl = document.createElement('span');
     tagEl.classList.add('tag');
     tagEl.innerText = tag; // 텍스트로 입력된 각각의 배열들
@@ -52,7 +57,27 @@ function randomSelect() {
     setTimeout(() => {
       const randomTag = pickRandomTag();
       highlightTag(randomTag);
-    }, 100)
+    }, 100);
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+      highlightTag(randomTag);
+    }, 100);
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+      highlightTag(randomTag);
+    }, 100);
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+      highlightTag(randomTag);
+    }, 100);
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+      highlightTag(randomTag);
+    }, 100);
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+      highlightTag(randomTag);
+    }, 100);
   }, times * 100);
 }
 
@@ -69,12 +94,9 @@ function unHighlightTag($tag) {
   $tag.classList.remove('highlight');
 }
 
-
 // function pickMe () {
 //   setTimeout(() => {
 //     const randomTag = pickRandomTag();
 //     highlightTag(randomTag);
 //   }, 100)
 // }
-
-
